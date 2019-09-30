958e284e74687d809958e96607fd2555eea765fa
Jayesh Tembhekar <jtembhekar2@gmail.com>
2019-09-30 14:05:33 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var slider = document.getElementById('slider');
+var element = document.getElementById('result');
+
+element.innerHTML = slider.value;
+
+slider.oninput = function () {
+    element.innerHTML = this.value;
+};
